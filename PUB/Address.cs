09ba0c2a0aadc28a4f683ff1fd5a42281ecfb086
Анастасия <Nastya20001492@gmail.com>
2019-09-26 14:18:12 +0300

    classes Address and Card

---
+++ PUB/Address.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUB
+{
+    class Address
+    {
+        private string street;
+        private int house;
+        private int flat;
+
+        public Address(string street,int house,int flat)
+        {
+            this.street = street;
+            this.house = house;
+            this.flat = flat;
+        }
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+
+        }
+        public int House
+        {
+            get { return house; }
+            set { house = value; }
+
+        }
+        public int Flat
+        {
+            get { return flat; }
+            set { flat = value; }
+
+        }
+    }
+   
+}
---
+++ PUB/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PUB
+{
+    class Card
+    {
+        private string id;
+        private double cardBalance;
+        public Card(string id, double cardBalance)
+        {
+            this.id = id;
+            this.cardBalance = cardBalance;
+        }
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+
+        }
+        public double CardBalance
+        {
+            get { return cardBalance; }
+            set { cardBalance = value; }
+
+        }
+    }
+    
+}
